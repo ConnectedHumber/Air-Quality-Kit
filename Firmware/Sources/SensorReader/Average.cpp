12adcdceed594a9eb4c42b7c05e053b9330fcf01
Brian <brian.n.norman@gmail.com>
2019-08-01 08:24:04 +0100

    First Commit
    Original Author: CrazyRobMiles

---
+++ Firmware/Sources/SensorReader/Average.cpp
@@ -0,0 +1,52 @@
+#include "Average.h"
+#include <Arduino.h>
+
+
+void Average::setNoOfValuesToAverage(int no)
+{
+	NoOfValuesToAverage = no;
+	restartAverage();
+}
+
+bool Average::addToAverage(float value)
+{
+	if (NoOfValuesToAverage == averageCount)
+		return false;
+
+	total += value;
+	averageCount++;
+
+	return true;
+}
+
+bool Average::averageReady()
+{
+	return NoOfValuesToAverage == averageCount;
+}
+
+bool Average::getAverage(float * result)
+{
+	if (!averageReady()) return false;
+
+	*result = total / averageCount;
+
+	restartAverage();
+
+	return true;
+}
+
+void Average::restartAverage()
+{
+	total = 0;
+	averageCount = 0;
+}
+
+Average::Average(int NoOfValuesToAverage)
+{
+	setNoOfValuesToAverage(NoOfValuesToAverage);
+}
+
+
+Average::~Average()
+{
+}
---
+++ Firmware/Sources/SensorReader/Average.h
@@ -0,0 +1,26 @@
+#pragma once
+class Average
+{
+private:
+
+
+	float average;
+	float total;
+	int averageCount;
+	int NoOfValuesToAverage;
+
+public:
+
+	void setNoOfValuesToAverage(int NoOfValuesToAverage);
+	bool addToAverage(float value);
+
+	void restartAverage();
+
+	bool averageReady();
+
+	bool getAverage(float * result);
+
+	Average(int NoOfValuesToAverage);
+	~Average();
+};
+
